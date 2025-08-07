12db19c174b944e5fa7316ea2eed505d3eb023ba
Heartbeast <heartbeast.studios@gmail.com>
2025-08-07 11:51:08 -0600

    5d - Line of Sight

--- enemies/bat_enemy.gd
+++ enemies/bat_enemy.gd
@@ -7,6 +7,7 @@ const SPEED = 30
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
 
 func _physics_process(delta: float) -> void:
 	var state = playback.get_current_node()
@@ -15,8 +16,12 @@ func _physics_process(delta: float) -> void:
 		"Chase":
 			var player = get_player()
 			if player is Player:
-				velocity = global_position.direction_to(player.global_position) * SPEED
-				sprite_2d.scale.x = sign(velocity.x)
+				var distance_to_player = global_position.distance_to(player.global_position)
+				if distance_to_player > 4:
+					velocity = global_position.direction_to(player.global_position) * SPEED
+					sprite_2d.scale.x = sign(velocity.x)
+				else:
+					velocity = Vector2.ZERO
 			else:
 				velocity = Vector2.ZERO
 			move_and_slide()
@@ -32,3 +37,14 @@ func is_player_in_range() -> bool:
 		if distance_to_player < range:
 			result = true
 	return result
+
+func can_see_player() -> bool:
+	var is_in_range = is_player_in_range()
+	if not is_in_range: return false
+	var player: = get_player()
+	if player is not Player: return false
+	ray_cast_2d.target_position = player.global_position - global_position
+	ray_cast_2d.force_raycast_update()
+	var is_player_in_line_of_sight = not ray_cast_2d.is_colliding()
+	return is_player_in_line_of_sight
+	
